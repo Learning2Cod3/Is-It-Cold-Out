f5892ea3721c1edaf6143eb63a225404481b1cb8
Omar Kange (BlackHat) <omarkange22@gmail.com>
2022-02-13 09:11:48 -0500

    test commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Real-Feel-Weather-App
+# Description
+This is a unqiue weather application, when you access the site, you are greated with photos that relate to the specific city you search
+# Table of Contents 
+* [installation](#installation)
+* [usage](#usage)
+* [License](#license)
+* [contributing](#contributing)
+* [tests](#tests)
+* [contact](#contact)
+* [github](#github)
+* [questions](#questions)
+# Installation
+The following necessary dependencies must be installed to run the application properly: None
+# Usage
+In order to use this app you need: It works by typing out the city you wish to gather weather information for. Zip code is not an options just yet, 5 day forcast isn't an option ether
+# License
+This project is licensed under the LMS license. 
+([![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+# contributing
+contributing: No, just me.
+# Tests
+The following is needed to run the test: basic HTML web access
+# Contact
+If you would like any additional detail, please follow the instructions on how you can reach out: Email
+# Questions
+Github username: https://github.com/Learning2cod3/Real-Feel-Weather-App
+
+If you have any questions about the repo, open an issue or have general questions you can reach me directly at: omarkange22@gmail.com 
---
+++ script.js
@@ -0,0 +1,66 @@
+//calling the weather api 
+
+let weather = {
+    apiKey: "6bef152439f592d693d0470f35cb6fe6",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+   
+   // to display the weather. I couldn't figure how to convert the weather to F instead of C 
+   // Everytime I change something thinking it's the solution, it ends up calling the error function above ^ "No weather found"
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+   
+   // to search for the weather in specific cities 
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  // adding event listner for search city function 
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+
+  weather.fetchWeather("Detroit");
+
+  
